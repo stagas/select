c302bdb69cb7989d91f28c4e4d66176218f4b73b
George Stagas <gstagas@gmail.com>
2013-02-28 07:35:22 +0200

    run in this tick and fix ie

--- index.js
+++ index.js
@@ -12,12 +12,6 @@
 module.exports = select;
 
 /**
- * Selection timeout.
- */
-
-var selectionTimeout;
-
-/**
  * Create a text range selection on `input`
  * from `start` to `end`.
  *
@@ -28,8 +22,6 @@ var selectionTimeout;
  */
 
 function select (input, start, end) {
-  input.focus();
-
   var value = input.value;
   var length = value.length;
 
@@ -42,24 +34,15 @@ function select (input, start, end) {
     break
   }
 
-  // selecting the text needs to happen in a new tick... :(
-  // https://code.google.com/p/chromium/issues/detail?id=32865
-  // http://stackoverflow.com/questions/11723420/chrome-setselectionrange-not-work-in-oninput-handler
-  clearTimeout(selectionTimeout);
-  selectionTimeout = setTimeout(function(){
-    if (input.createTextRange) {
-      // use text ranges for Internet Explorer
-      var range = input.createTextRange();
-      range.moveStart('character', start);
-      range.moveEnd('character', end - start);
-      range.select();
-    }
-    else if (input.setSelectionRange) {
-      // use setSelectionRange() for Mozilla/WebKit
-      input.setSelectionRange(start, end);
-    }
-
-    // set focus back to the input el
-    input.focus();
-  }, 0);
+  if (input.createTextRange) {
+    // use text ranges for Internet Explorer
+    var range = input.createTextRange();
+    range.moveStart('character', start);
+    range.moveEnd('character', end - length);
+    range.select();
+  }
+  else if (input.setSelectionRange) {
+    // use setSelectionRange() for Mozilla/WebKit
+    input.setSelectionRange(start, end);
+  }
 };
